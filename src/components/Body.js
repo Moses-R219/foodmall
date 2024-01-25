18649c6bb164edc30048a47961597e2b3b1d6036
MOSES R <mosesrafel@gmail.com>
2024-01-25 10:55:28 +0530

    Live api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,46 @@
+import { useEffect, useState } from "react";
 import {  restaurantList } from "../../config";
 import RestaurantCard from "./RestaurantCard";
 
 
+const filterrestaurant=(search,restaurants)=>{
+    const fi= restaurants.filter((res)=>(res.name).includes(search));
+    return fi;
+}
+
 const Body=()=>{
+    
+    const [restaurants,setRestaurants]=useState([])
+    const [search,setSearch]=useState("");
+
+    useEffect(()=>{
+        getFoods();
+    },[]);
+
+    async function getFoods(){
+        const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.3355423&lng=77.7143421&page_type=DESKTOP_WEB_LISTING")
+        const json=await data.json();
+        //data.length === 12 cards
+
+        console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle);
+        
+        setRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+   
 return(
     <>
     <div className="search">
-        <input type="text" placeholder="search" value={""} onChange={onchange()}/>
-        <button>Search</button>
+        <input type="text" placeholder="search" value={search} onChange={(e)=>setSearch(e.target.value)}/>
+        <button onClick={()=>{
+            const filtered=filterrestaurant(search,restaurants);
+            setRestaurants(filtered)
+        }}>Search</button>
     </div>
     <div className="restaurant-list">
         {
-            restaurantList.map((restaurant)=>{
-                return <RestaurantCard {...restaurant} key={restaurant.id}/>
+            restaurants.map((restaurant)=>{
+                return <RestaurantCard {...restaurant.info} key={restaurant.info.id}/>
             })  
         }
     </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,9 +3,9 @@ import { ImageUrl } from "../../config";
 const RestaurantCard=({name,cloudinaryImageId,cuisines})=>{
     return(
         <div className="card">
-            <img src={ImageUrl+cloudinaryImageId} alt="restaurant"/>
+            <img className="size" src={ImageUrl+cloudinaryImageId} alt="restaurant"/>
             <h4>{name}</h4>
-            <h4>{cuisines.join(",")}</h4>
+            <span className="wrap">{cuisines.join(",")}</span>
         </div>
     )
 }
